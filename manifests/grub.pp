b58aed158423f93fc246957bee1f0c2e318738fa
Ahmed Osman <ahmed.osman@nordcloud.com>
2016-03-21 17:01:52 +0100

    Enforce grub.cfg permissions Move secure condition to grub manifest

--- manifests/grub.pp
+++ manifests/grub.pp
@@ -1,22 +1,28 @@
-class cis::grub {
-  require Class['auditd']
+class cis::grub ($secure_grub = $cis::params::secure_grub) inherits cis::params {
+  require cis::auditd_rules
 
-  $grub_secret = hiera('grub_encrpted')
-  $grub_password = hiera('grub_clear')
+  if $secure_grub {
+    $grub_secret = hiera('grub_encrpted')
+    $grub_password = hiera('grub_clear')
 
-  # Correct all kernel lines to have the needed console parameters
-  augeas { "grub-set-kernel-consoles":
-    context => "/files/boot/grub/menu.lst",
-    changes => ["set /files/boot/grub/menu.lst/title/kernel/ audit=1",],
+    # Correct all kernel lines to have the needed console parameters
+    augeas { "grub-set-kernel-consoles":
+      context => "/files/boot/grub/menu.lst",
+      changes => ["set /files/boot/grub/menu.lst/title/kernel/ audit=1",],
+    }
+
+    augeas { "Add SHA-512 password to Grub":
+      context => "/files/boot/grub/menu.lst",
+      changes => ["ins password after timeout", "${grub_secret}", "${grub_password}",],
+      onlyif  => "match password size == 0";
+    }
   }
 
-  augeas { "Add SHA-512 password to Grub":
-    context => "/files/boot/grub/menu.lst",
-    changes => ["ins password after timeout", "${grub_secret}", "${grub_password}",],
-    onlyif  => "match password size == 0";
+  file { '/boot/grub2/grub.cfg':
+    owner => 'root',
+    group => 'root',
+    mode  => '0640',
   }
-
-
 
 
 }
