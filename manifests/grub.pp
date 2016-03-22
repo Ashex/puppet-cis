46ea39fce03528ffe818e69e0f85f5a04497313d
Ahmed Osman <ahmed.osman@nordcloud.com>
2016-03-22 15:59:50 +0100

    Removed legacy grub configuration (not application in v1.1.0 of CIS guide)

--- manifests/grub.pp
+++ manifests/grub.pp
@@ -1,22 +1,4 @@
-class cis::grub ($secure_grub = $cis::params::secure_grub) inherits cis::params {
-  require cis::auditd_rules
-
-  if $secure_grub {
-    $grub_secret = hiera('grub_encrpted')
-    $grub_password = hiera('grub_clear')
-
-    # Correct all kernel lines to have the needed console parameters
-    augeas { "grub-set-kernel-consoles":
-      context => "/files/boot/grub/menu.lst",
-      changes => ["set /files/boot/grub/menu.lst/title/kernel/ audit=1",],
-    }
-
-    augeas { "Add SHA-512 password to Grub":
-      context => "/files/boot/grub/menu.lst",
-      changes => ["ins password after timeout", "${grub_secret}", "${grub_password}",],
-      onlyif  => "match password size == 0";
-    }
-  }
+class cis::grub {
 
   file { '/boot/grub2/grub.cfg':
     owner => 'root',
