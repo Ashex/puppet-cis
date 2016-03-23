c02763e0f4bffcb9a197092140aa358ffbfa2059
Ahmed Osman <ahmed.osman@nordcloud.com>
2016-03-23 13:21:37 +0100

    Update grub with audit option

--- manifests/grub.pp
+++ manifests/grub.pp
@@ -6,5 +6,24 @@ class cis::grub {
     mode  => '0640',
   }
 
+  shellvar { "GRUB_CMDLINE_LINUX":
+	  ensure       => present,
+	  target       => "/etc/default/grub",
+	  value        => "audit=1",
+	  array_append => true,
+	  require 	   => Class['cis::auditd_rules'],
+	  notify       => Exec['grub-update'],
+	}
+
+  exec { 'grub-update':
+	  refreshonly => true,
+	  command 	  => '/sbin/grub2-mkconfig -o /boot/grub2/grub.cfg',
+      require     => Package['grub2-tools-install'],
+	}
+
+package {'grub2-tools-install':
+    name => 'grub2-tools',
+    ensure => installed,
+  }
 
 }
